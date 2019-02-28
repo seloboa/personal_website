15cf68003673f0390555d3ef521ddc3d03ef0bab
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-02-27 20:52:19 -0500

    finish config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  entry: 'index.js',
+  mode:'production',
+  entry: './index.js',
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
+    path: __dirname+'./public',
+    filename: 'bundle.js'
   },
 }
