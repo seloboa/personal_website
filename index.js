e4f4744d0fe4cba54110c7b54c2d49a6d3b7d982
Bao Nguyen <bao.ng.nguyen3@gmail.com>
2019-02-27 23:21:42 -0500

    added react dom, made webpack works with babel for jsx

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class Main extends Component{
-  constructor(){
+class Main extends Component {
+  constructor() {
     super();
   }
-  render(){
-    return{
-
-    }
+  render() {
+    return (
+      <div class="backgroundWrapper">
+        <div class="textWrapper">
+          <div class="mainText">
+            <p id="name" class="fadeinanimated">
+              Bao Nguyen
+            </p>
+            <p id="title" class="fadeinanimated">
+              Digital Marketer | Developer
+            </p>
+            <p id="location" class="fadeinanimated">
+              New York City
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
